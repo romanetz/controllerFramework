9ed107bf4ff9a3142218486cb17e062a8a2da26d
kiv <kiv.apple@gmail.com>
2015-11-12 03:44:59 +0300

    Added Linux platform

---
+++ platform/linux/linux.h
@@ -0,0 +1,18 @@
+#ifndef __LINUX_H__
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+#include <sys/fcntl.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stropts.h>
+#include <sys/ioctl.h>
+
+#include <linux-filechannel.h>
+#include <linux-serialport.h>
+
+#endif
---
+++ platform/linux/rules.mk
@@ -0,0 +1,18 @@
+TARGET ?=
+
+SOURCES += $(wildcard $(PLATFORM_DIR)/*.cpp)
+INCLUDE_PATH += $(PLATFORM_DIR)
+
+LFLAGS += -lstdc++
+
+OUTPUT_FILES = $(BUILD_DIR)/$(PROJECT_NAME).elf
+
+$(BUILD_DIR)/%.elf: $(OBJECTS) Makefile
+	@echo "Linking $(notdir $@)..."
+	$(Q)$(LD) $(CFLAGS) -o $@ $(OBJECTS) $(LFLAGS)
+
+all: $(BUILD_DIR) $(OUTPUT_FILES)
+	$(Q)$(SIZE) $(BUILD_DIR)/$(PROJECT_NAME).elf
+
+run: $(BUILD_DIR)/$(PROJECT_NAME).elf
+	$(BUILD_DIR)/$(PROJECT_NAME).elf
